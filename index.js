152d3480d9046abbc0d83a354676f41699592055
lenus254 <linus.cheruiyot@student.moringaschool.com>
2021-11-29 10:02:38 +0300

    modified total

--- index.js
+++ index.js
@@ -14,44 +14,44 @@ function pizza(name){
         function Crusttype(name){
             this.crustName=name
             }
-    Crusttype.prototype.crustSelected=function(){
+        Crusttype.prototype.crustSelected=function(){
                 return this.crustName
             }
-            function Toppingtype(name){
+         function Toppingtype(name){
                 this.toppingName=name
                 }
         Toppingtype.prototype.toppingSelected=function(){
                     return this.toppingName
-                }   
-                $(document).ready(function(){
-                    $(document).on("click","#btn",function(event){
-                        event.preventDefault()
-                        
-                        var myPizzaSize=$("#pizzaSize option:selected").text()
-                        var myPizzaType=$("#pizzaType option:selected").text()
-                        var myCrustType=$("#crusttype option:selected").text()
-                        var myToppingType=$("#toppingtype option:selected").text()  
-                        
-                        var myPizzaValue=parseInt($("#pizzaSize option:selected").val())
-                        var myCrustValue=parseInt($("#crusttype option:selected").val())
-                        var myToppingValue=parseInt($("#toppingtype option:selected").val())
-                        var pizzaQuantity=$("#pizzanumber").val()
-                        var finalPizzaPrice=(pizzaQuantity)*(myPizzaValue+myCrustValue+myToppingValue)
-
-                        var myPizzaChoice=new pizza(myPizzaType)
-                        var mySizeChoice=new pizzatype(myPizzaSize)
-                        var myCrustChoice=new Crusttype(myCrustType)
-                        var myToppingChoice=new Toppingtype(myToppingType)
-
-                        $("#output").html("<div>"+"Your Pizza Choice is: "+myPizzaChoice.pizzaSelected()+"<br>"+"Selected Size:"+mySizeChoice.sizeSelected()+"<br>"+"Crust Selected:"+myCrustChoice.crustSelected()+"<br>"+"Topping Selected:  "+myToppingChoice.toppingSelected()+"<br>"+"Total Pizza Price:  "+finalPizzaPrice+"<br>"+" <button id='btnDelivery'>Delivery</button>"+"</div>")
-                    })
-                    $(document).on("click","#btnDelivery",function(){
-                       let DeliveryLocation= prompt("Where do you want your order to be delivered?")
-                       if(!DeliveryLocation){
-                           alert("Please fill in Your Location!")
-                       }
-                       else{
-                           alert("You will have to pay Ksh.150 extra for delivery")
-                       }
-                    })
-                })
+                }
+    $(document).ready(function(){
+        $(document).on("click","#btn",function(event){
+            event.preventDefault()
+            
+            var myPizzaSize=$("#pizzaSize option:selected").text()
+            var myPizzaType=$("#pizzaType option:selected").text()
+            var myCrustType=$("#crusttype option:selected").text()
+            var myToppingType=$("#toppingtype option:selected").text()
+    
+            var myPizzaValue=parseInt($("#pizzaSize option:selected").val())
+            var myCrustValue=parseInt($("#crusttype option:selected").val())
+            var myToppingValue=parseInt($("#toppingtype option:selected").val())
+            var pizzaQuantity=$("#pizzanumber").val()
+            var finalPizzaPrice=(pizzaQuantity)*(myPizzaValue+myCrustValue+myToppingValue)
+          //  alert( finalPizzaPrice)
+            var myPizzaChoice=new pizza(myPizzaType)
+            var mySizeChoice=new pizzatype(myPizzaSize)
+            var myCrustChoice=new Crusttype(myCrustType)
+            var myToppingChoice=new Toppingtype(myToppingType)
+          //  alert(mySizeChoice)
+            $("#output").html("<div>"+"Your Pizza Choice is: "+myPizzaChoice.pizzaSelected()+"<br>"+"Selected Size:"+mySizeChoice.sizeSelected()+"<br>"+"Crust Selected:"+myCrustChoice.crustSelected()+"<br>"+"Topping Selected:  "+myToppingChoice.toppingSelected()+"<br>"+"Total Pizza Price:  "+finalPizzaPrice+"<br>"+" <button id='btnDelivery'>Delivery</button>"+"</div>")
+        })
+        $(document).on("click","#btnDelivery",function(){
+           let DeliveryLocation= prompt("Where do you want your order to be delivered?")
+           if(!DeliveryLocation){
+               alert("Please fill in Your Location!")
+           }
+           else{
+               alert("You will have to pay Ksh.150 extra for delivery")
+           }
+        })
+    })
